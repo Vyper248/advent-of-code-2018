596ee42d2e65ce947f968f0227724c2d1afc7529
Vyper248 <vyper375@hotmail.com>
2018-12-01 16:57:28 +0000

    day 1 finished

--- day1.js
+++ day1.js
@@ -1,11 +1,29 @@
 const readFile = require('./readFile').readFile;
 
 const first = (input) => {
-    
+    let frequency = input.split('\n').reduce((a, c) => {
+        return a + Number(c);
+    }, 0);
+    console.log('First Star: ', frequency);
 };
 
 const second = (input) => {
+    const freqs = input.split('\n');
+    const seen = {0: 1};
+    let currentFreq = 0;
+    let repeat = false;
     
+    while(!repeat){
+        for (let i = 0; i < freqs.length; i++){
+            currentFreq += Number(freqs[i]);
+            if (seen[currentFreq] === 1) {
+                console.log('Second Star: ', currentFreq);
+                repeat = true;
+                break;
+            }
+            seen[currentFreq] = 1;
+        };
+    }
 };
 
 readFile('day1Input.txt', first);
