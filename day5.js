9c87842b72d2c0524264ae8723fb403c6a0fb9a3
Vyper248 <vyper375@hotmail.com>
2018-12-05 12:11:13 +0000

    Completed day 5

--- day5.js
+++ day5.js
@@ -6,11 +6,35 @@ const runFunctions = (input) => {
 }
 
 const first = (input) => {
-    
+    const final = removePairs(input);
+    console.log('First Star: ', final.length);
 };
 
 const second = (input) => {
-    
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    let shortest = 1000000;
+    letters.split('').forEach(letter => {
+        let regex = new RegExp(letter, 'gi');
+        const removed = input.replace(regex, '');
+        const final = removePairs(removed);
+        if (final.length < shortest) shortest = final.length;
+    });
+    console.log('Second Star: ', shortest);
 };
+
+function removePairs(string){
+    const initialString = string;
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    letters.split('').forEach(letter => {
+        let regex = new RegExp(letter+letter.toUpperCase(), 'g');
+        let regex2 = new RegExp(letter.toUpperCase()+letter, 'g');
+        string = string.replace(regex, '');
+        string = string.replace(regex2, '');
+    });
+
+    if (string.length < initialString.length) string = removePairs(string);
+
+    return string;
+}
 
 readFile('day5Input.txt', runFunctions);
