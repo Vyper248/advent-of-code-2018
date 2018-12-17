76d367cb72858d142869a31fbf7bfad9d80a24ed
Vyper248 <vyper375@hotmail.com>
2018-12-17 11:51:09 +0000

    Cleaned Day 17

--- day17.js
+++ day17.js
@@ -9,7 +9,7 @@ const first = (input) => {
         
     flow(grid, lowX, highX, highY, 500, 0);
     
-    display(grid, lowX, highX, 0);
+    // display(grid, lowX, highX, 0);
     
     let water = countWater(grid, lowX, highX, lowY, highY);
     console.log('First Star: ', water);
@@ -51,27 +51,18 @@ const retained = (grid, lowX, highX, lowY, highY) => {
 }
 
 const flow = (grid, lowX, highX, highY, x, y) => {
-    // console.log('Flowing from: ', x, y);
     let bottom = findClay(grid, x, y, highY);
-    // console.log('Found clay at: ', x, bottom);
+    fall(grid, x, y, bottom);//let water fall down to bottom position
     
-    // console.log('Falling from ', y, ' to ', bottom, ' at x: ', x);
-    if (grid[x][bottom+1] === '|') {
-        fall(grid, x, y, bottom);
-        return;
-    }
+    if (grid[x][bottom+1] === '|') return; //if hitting water, then fall to it and stop (will merge with it) 
+    if (bottom === highY) return; //if hitting bottom of grid, stop
     
-    fall(grid, x, y, bottom);
-    
-    if (bottom === highY) return;
-    
+    //while within a contained area, fill with water, otherwise get positions to spread to
     let spreadFrom, spreadTo;
-    
     while (true){
         [canFill, from, to] = checkContained(lowX, highX, grid, x, bottom);
         if (canFill){
-            // console.log('Filling from: ', from, ' to: ', to, ' at y: ', bottom);
-            fill(grid, bottom, from, to);
+            fillWith(grid, bottom, from, to, '~');
             bottom--;
         } else {
             spreadFrom = from;
@@ -80,11 +71,12 @@ const flow = (grid, lowX, highX, highY, x, y) => {
         }
     }
     
-    // console.log('Spreading from: ', spreadFrom, ' to: ', spreadTo, ' bottom is: ', bottom);
-    spread(grid, bottom, spreadFrom, spreadTo);
+    //spread water
+    fillWith(grid, bottom, spreadFrom, spreadTo, '|');
     
-    if (grid[spreadFrom][bottom+1] === '.' && bottom+1 < highY) flow(grid, lowX, highX, highY, spreadFrom, bottom);
-    if (grid[spreadTo][bottom+1] === '.' && bottom+1 < highY) flow(grid, lowX, highX, highY, spreadTo, bottom);
+    //using end positions of spread, begin flow again
+    if (grid[spreadFrom][bottom+1] === '.') flow(grid, lowX, highX, highY, spreadFrom, bottom);
+    if (grid[spreadTo][bottom+1] === '.') flow(grid, lowX, highX, highY, spreadTo, bottom);
 };
 
 const fall = (grid, x, from, to) => {
@@ -93,62 +85,58 @@ const fall = (grid, x, from, to) => {
     }
 };
 
-const fill = (grid, y, from, to) => {
+const fillWith = (grid, y, from, to, str) => {
     for (let x = from; x <= to; x++){
-        grid[x][y] = '~';
-    }
-};
-
-const spread = (grid, y, from, to) => {
-    for (let x = from; x <= to; x++){
-        grid[x][y] = '|';
+        grid[x][y] = str;
     }
 };
 
 const findClay = (grid, x, y, highY) => {
     for (let i = y+1; i <= highY; i++){
-        if (hitClay(grid, x, i)) return i;
+        if (hitClayOrWater(grid, x, i)) return i;
     }
     return highY;
 };
 
-const hitClay = (grid, x, y) => {
+const hitClayOrWater = (grid, x, y) => {
     if (grid[x][y+1] === '#' || grid[x][y+1] === '|') return true;
     return false;
 };
 
+//check if inside a contained area and return that area. If not, return positions to spread from and to
 const checkContained = (lowX, highX, grid, x, y) => {
-    let onRight = false;
-    let to = highX;
     let onLeft = false;
     let from = lowX;
-    for (let i = x+1; i <= highX+4; i++){
-        if (grid[i][y] === '#'){//found a clay wall to the right
-            onRight = true;
-            to = i-1;
-            break;
-        }
-        if (grid[i][y+1] === '.' || grid[i][y+1] === '|'){//found a hole to the right, so won't be contained
-            to = i;
-            break;
-        }
+    let onRight = false;
+    let to = highX;
+    
+    //check to the left of position
+    for (let i = x-1; i >= lowX-4; i--){
+        [onLeft, from, foundOther] = checkPosition(grid, i, y, lowX, 1);
+        if (onLeft) break;
+        if (foundOther) break;
     }
     
-    for (let i = x-1; i >= lowX-4; i--){
-        if (grid[i][y] === '#'){//found a clay wall to the right
-            onLeft = true;
-            from = i+1;
-            break;
-        }
-        if (grid[i][y+1] === '.' || grid[i][y+1] === '|'){//found a hole to the left, so won't be contained
-            from = i;
-            break;
-        }
+    //check to the right of position
+    for (let i = x+1; i <= highX+4; i++){
+        [onRight, to, foundOther] = checkPosition(grid, i, y, highX, -1);
+        if (onRight) break;
+        if (foundOther) break;
     }
     
     if (onRight && onLeft) return [true, from, to];
     return [false, from, to];
 };
+
+const checkPosition = (grid, x, y, defPos, dir) => {
+    if (grid[x][y] === '#'){//found a clay wall to the right
+        return [true, x+dir, false];
+    }
+    if (grid[x][y+1] === '.' || grid[x][y+1] === '|'){//found a hole or water to the left, so won't be contained
+        return [false, x, true];
+    }
+    return [false, defPos, false];
+}
 
 const getGrid = (input) => {
     let highX = 0;
@@ -160,33 +148,16 @@ const getGrid = (input) => {
     let yLines = [];
     
     input.split('\n').forEach(line => {
-        //x=464, y=786..795
-        //y=1588, x=414..417
-        
         const parts = line.split(',');
         if (parts[0].includes('x')){
             const x = Number(parts[0].replace('x=', ''));
-            if (x > highX) highX = x;
-            if (x < lowX) lowX = x;
-            const yParts = parts[1].replace('y=', '');
-            const from = Number(yParts.split('..')[0]);
-            if (from > highY) highY = from;
-            if (from < lowY) lowY = from;
-            const to = Number(yParts.split('..')[1]);
-            if (to > highY) highY = to;
-            if (from < lowY) lowY = from;
+            const [from, to] = parts[1].replace(/[x|y]=/, '').split('..').map(Number);
+            [lowX, highX, lowY, highY] = compareValues(x, lowX, highX, from, to, lowY, highY);
             xLines.push({x, from, to});
         } else {
             const y = Number(parts[0].replace('y=', ''));
-            if (y > highY) highY = y;
-            if (y < lowY) lowY = y;
-            const xParts = parts[1].replace('x=', '');
-            const from = Number(xParts.split('..')[0]);
-            if (from > highX) highX = from;
-            if (from < lowX) lowX = from;
-            const to = Number(xParts.split('..')[1]);
-            if (to > highX) highX = to;
-            if (from < lowX) lowX = from;
+            const [from, to] = parts[1].replace(/[x|y]=/, '').split('..').map(Number);
+            [lowX, highX, lowY, highY] = compareValues(y, lowY, highY, from, to, lowX, highX);
             yLines.push({y, from, to});
         }
     });
@@ -219,5 +190,13 @@ const getGrid = (input) => {
         
     return [grid, lowX, highX, lowY, highY];
 };
+
+const compareValues = (a, lowA, highA, from, to, lowB, highB) => {
+    if (a > highA) highA = a;
+    if (a < lowA) lowA = a;
+    if (from < lowB) lowB = from;
+    if (to > highB) highB = to;
+    return [lowA, highA, lowB, highB];
+}
 
 readFile('day17Input.txt', runFunctions);
